f5754432b8996b3167513b2374078d337ec551b9
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-29 10:35:21 +0530

    set nx workspace options

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { Module } from "@nestjs/common";
-import { PrismaModule } from "./prisma/prisma.module";
-import {GraphQLModule} from "@nestjs/graphql";
-import {ApolloDriverConfig, ApolloDriver} from "@nestjs/apollo";
-import { UsersModule } from "./users/users.module";
-
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports : [
+  imports: [
+    ConfigModule,
     PrismaModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    UsersModule
+    UsersModule,
   ],
-  providers : []
+  providers: [],
 })
 export class AppModule {}
--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,7 @@ async function bootstrap() {
     })
   );
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = app.get(ConfigService).getOrThrow('AUTH_PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
