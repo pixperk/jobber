c8e1e35ff9d103b302b64c0f93f4484c9b7bd549
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-31 11:23:06 +0530

    Metadata discovery

--- apps/jobber-jobs/src/app/jobs/jobs.module.ts
+++ apps/jobber-jobs/src/app/jobs/jobs.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FibonacciJob } from './fibonacci.job';
+import { DiscoveryModule } from '@golevelup/nestjs-discovery';
+import { JobsService } from './jobs.service';
 
 @Module({
-  providers: [FibonacciJob],
+  imports: [DiscoveryModule],
+  providers: [FibonacciJob, JobsService],
 })
 export class JobsModule {}
