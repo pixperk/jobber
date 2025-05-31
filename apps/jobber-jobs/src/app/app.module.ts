c9bd7727fcea12b6f3f71c4c92d6aa5438d8b372
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2025-05-31 12:07:11 +0530

    job resolver

--- apps/jobber-jobs/src/app/app.module.ts
+++ apps/jobber-jobs/src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { JobsModule } from './jobs/jobs.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule, JobsModule],
+  imports: [
+    ConfigModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+    }),
+    JobsModule,
+  ],
   controllers: [],
   providers: [],
 })
